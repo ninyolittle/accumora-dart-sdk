7f9d7250c3252194f0a38a24af24107c8c866e9f
Ninolito <ninolitopeligrino@gmail.com>
2025-09-21 15:26:59 +0800

    Refactor library exports and remove unused base file

--- lib/accumora_dart_sdk.dart
+++ lib/accumora_dart_sdk.dart
@@ -1,1 +1,11 @@
-// GENERATED EXPORTS
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'amor/amor.pb.dart';
+export 'amor/amor.pbenum.dart';
+export 'amor/amor.pbgrpc.dart';
+export 'amor/amor.pbjson.dart';
+
+// TODO: Export any libraries intended for clients of this package.
