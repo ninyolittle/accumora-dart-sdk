735f600ca78274c5ec46f3e5660013e24568b6da
Ninolito <ninolitopeligrino@gmail.com>
2025-12-19 15:43:18 +0800

    Remove commented-out makeRequest method from AmorAPIClient

--- lib/amor/v1/client.dart
+++ lib/amor/v1/client.dart
@@ -20,48 +20,6 @@ class AmorAPIClient {
 
   ProjectAmorClient get stub => _stub;
 
-  // Future<T?> makeRequest<T, K>(
-  //   Future<T> Function(K, {CallOptions? options}) rpcMethod,
-  //   K request,
-  //   MakeRequestOpts? opts,
-  // ) async {
-  //   try {
-  //     final accessToken = opts?.token ?? await storage.read(key: ACCESS_TOKEN);
-  //     final options = accessToken != null
-  //         ? CallOptions(metadata: {'authorization': 'Bearer $accessToken'})
-  //         : null;
-
-  //     return await rpcMethod(request, options: options);
-  //   } catch (e) {
-  //     if (opts != null && opts.context.mounted) {
-  //       if (opts.skipErrorDisplay) {
-  //         return null;
-  //       }
-
-  //       if (e is GrpcError) {
-  //         AccumoraAlertModal.show(
-  //           isDestructiveAction: true,
-  //           context: opts.context,
-  //           title: e.codeName == "INTERNAL"
-  //               ? e.codeName.toLowerCase()
-  //               : opts.errorTitle,
-  //           content: e.message ?? opts.errorMsg,
-  //           hideCancel: true,
-  //         );
-  //       } else {
-  //         AccumoraAlertModal.show(
-  //           isDestructiveAction: true,
-  //           context: opts.context,
-  //           title: opts.errorTitle,
-  //           content: opts.errorMsg,
-  //           hideCancel: true,
-  //         );
-  //       }
-  //     }
-  //   }
-  //   return null;
-  // }
-
   Future<void> shutdown() async {
     await _channel.shutdown();
   }
